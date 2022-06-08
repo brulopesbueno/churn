ede39baa92b8efde9e3b47d84da66c7c08e451ef
Bruna Bueno <55333509+brulopesbueno@users.noreply.github.com>
2022-06-07 21:41:36 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,16 +7,35 @@ with open('model.pkl', 'rb') as file_in:
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html', nome='Fulano')
+def homepage():
+  return render_template('homepage.html', nome='Fulano')
 
 @app.route('/predicao', methods=['POST'])
 def predicao():
-  comentario = request.form['comentario']
-  predicao = model.predict([comentario])
-  return render_template('resposta.html', predicao=predicao[0])
-
+  Gender = int(request.form['Gender'])
+  Senior_Citizen = int(request.form['Senior_Citizen'])
+  Phone_Service = int(request.form['Phone_Service'])
+  Tech_Support = int(request.form['Tech_Support'])
+  Streaming_Movies = int(request.form['Streaming_Movies'])
+  Contract = int(request.form['Contract'])
+  Payment_Method = int(request.form['Payment_Method'])
+  predicao = model.predict(['Gender'])
+  predicao = model.predict(['Senior_Citizen'])
+  predicao = model.predict(['Phone_Service'])
+  predicao = model.predict(['Tech_Support'])
+  predicao = model.predict(['Streaming_Movies'])
+  predicao = model.predict(['Contract'])
+  predicao = model.predict(['Payment_Method'])
+  return render_template('predicao.html', predicao=predicao[0])
+  
+  
 app.run(debug=True)
 
 # pip install -r requirements.txt (instala as bibliotecas)
 # python app.py (para executar)
+
+# git add .
+
+# git commit -m "nomenovo"
+# git push
+
